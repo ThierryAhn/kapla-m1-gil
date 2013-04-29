9ba4b1a589cca3450b9328668db40222bf6c323b
folabi41 <folabi41@b3bcd212-3c03-7b25-41cb-3a5433dfaa12>
2013-04-29 11:56:13 +0000

    chargement des images effectués. mais quand il a beaucoup d'images, il a du mal. Je pense qu'il va falloir limiter le nombre d'images, je suis quand en train de chercher un autre moyen.

--- trunk/EditorKapla/src/model/notice/NoticeInterface.java
+++ trunk/EditorKapla/src/model/notice/NoticeInterface.java
@@ -3,15 +3,19 @@ package model.notice;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import java.awt.Dimension;
+
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -27,7 +31,7 @@ import javax.swing.JTextField;
  *
  */
 public class NoticeInterface extends JFrame{
-	
+
 	/**
 	 * Chemin du dossier des images.
 	 */
@@ -35,7 +39,8 @@ public class NoticeInterface extends JFrame{
 	/**
 	 * Panel des images.
 	 */
-	private JPanel centerPanel;
+	private JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 
+			PADDING, PADDING));
 	/**
 	 * Nombre d'images.
 	 */
@@ -44,32 +49,33 @@ public class NoticeInterface extends JFrame{
 	 * Liste contenant les images.
 	 */
 	private ArrayList<NoticeImage> arrayImages = new ArrayList<NoticeImage>();
-	
+
 	/**
 	 * Padding entre les images.
 	 */
-	private static final int PADDING = 3;
+	private static final int PADDING = 15;
 	/**
 	 * Taille des images.
 	 */
-	private int SIZE = 5;
-	
+	private static int WIDTH = 150;
+	private static int HEIGHT = 150;
+
 	public NoticeInterface(){
 		super("New Notice");
 		setLayout(new BorderLayout());
-		
-		
+
+
 		// panel de choix du dossier des images
 		JPanel northPanel = new JPanel(new BorderLayout());
-		
+
 		// champ de texte qui contient le chemin du repertoire des images
 		final JTextField folderPath = new JTextField();
 		folderPath.setEditable(false);
-		
+
 		// bouton de choix du dossier des images
 		JButton chooseFolder = new JButton("Choose Folder");
 		chooseFolder.addActionListener(new ActionListener(){
-			
+
 			public void actionPerformed(ActionEvent arg0) {
 				JFileChooser chooser = new JFileChooser();
 				chooser.setCurrentDirectory(new java.io.File("."));
@@ -78,82 +84,95 @@ public class NoticeInterface extends JFrame{
 				if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
 					folderPath.setText(chooser.getSelectedFile().toString());
 					imagesPath = chooser.getSelectedFile().toString();
-					
+
 					// recuperation des images du dossier
 					getFiles(imagesPath);
 				}
 				System.out.println((int) (countImages/5 + 1.0f));
-				
-				
-				// mise a jour du panel des images
-				//centerPanel = new JPanel(new GridLayout(7, 10));
-				centerPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 
-						PADDING, PADDING));
-				
-				for(NoticeImage noticeImage : arrayImages){
-					ImageIcon imageIcon = new ImageIcon(noticeImage.getPath());
 
-					Image img = imageIcon.getImage();  
-					BufferedImage bi = new BufferedImage(img.getWidth(null), 
-							img.getHeight(null), BufferedImage.TYPE_INT_ARGB);  
+
+				// mise a jour du panel des images
+				//centerPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 
+				//		PADDING, PADDING));
+				centerPanel.removeAll();
+				for(NoticeImage noticeImage : arrayImages){
+					try {
+						BufferedImage originalImage = ImageIO.read(new File(
+								noticeImage.getPath()));
+						BufferedImage resizeImage = resizeImage(
+								originalImage, BufferedImage.TYPE_INT_ARGB);
+						ImageIcon newImageIcon = new ImageIcon(resizeImage);
+
+						JLabel tempLabel = new JLabel(newImageIcon);
+						centerPanel.add(tempLabel);
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
 					
-					Graphics g = bi.createGraphics();  
-					g.drawImage(img, 140, 199, SIZE, SIZE, null, null);  
-					ImageIcon newImageIcon = new ImageIcon(bi);  
-					
-					
-					
-					JLabel tempLabel = new JLabel(newImageIcon);
-					//tempLabel.setMaximumSize(maximumSize);
-					//tempLabel.setSize(5, 5);
-					
-					centerPanel.add(tempLabel);
 				}
-				for(int i = countImages; i < 70; i++){
-					JLabel tempLabel = new JLabel();
-					//tempLabel.setMaximumSize(maximumSize);
-					tempLabel.setSize(5, 5);
-					centerPanel.add(tempLabel);
-				}
+				
+				centerPanel.repaint();
+				centerPanel.validate();
 				add(centerPanel);
 			}
-			
+
 		});
-				
+
 		// ajout des composants du northPanel
 		northPanel.add(folderPath);
 		northPanel.add(chooseFolder, BorderLayout.EAST);
-		
-		
-		
+
+
+
 		// ajout des panels
 		add(northPanel, BorderLayout.NORTH);
-		
+		add(centerPanel);
+
 		pack();
 		setVisible(true);
 	}
-	
+
 	/**
 	 * Liste les fichiers d'un dossier.
 	 * @param folder dossier dont il faut lister les fichiers.
 	 */
 	private void getFiles(String folder) {
-	    File file = new File(folder);
-	    File[] files = file.listFiles();
-	    
-	    arrayImages.clear();
-	    if (files != null) {
-	        for (int i = 0; i < files.length; i++) {
-	            if(files[i].isDirectory() == false) {
-	            	NoticeImage noticeImage = new NoticeImage(imagesPath +"\\"
-	            			+files[i].getName());
-	            	
-	            	arrayImages.add(noticeImage);
-	            	countImages++;
-	            }
-	        }
-	    }
+		File file = new File(folder);
+		File[] files = file.listFiles();
+
+		arrayImages.clear();
+		if (files != null) {
+			for (int i = 0; i < files.length; i++) {
+				if(files[i].isDirectory() == false) {
+					NoticeImage noticeImage = new NoticeImage(imagesPath +"\\"
+							+files[i].getName());
+
+					arrayImages.add(noticeImage);
+					countImages++;
+				}
+			}
+		}
 	}	
+
+	/**
+	 * Redimensionne une image. 
+	 * @param originalImage l'image d'origine
+	 * @param type type des variables WIDTH et HEIGHT
+	 * @return une image redimensionnee.
+	 */
+	private static BufferedImage resizeImage(BufferedImage originalImage, 
+			int type){
+		
+		BufferedImage resizedImage = new BufferedImage(WIDTH, HEIGHT, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, WIDTH, HEIGHT, null);
+		g.dispose();
+
+		return resizedImage;	
+	}
+
 }
 
 
