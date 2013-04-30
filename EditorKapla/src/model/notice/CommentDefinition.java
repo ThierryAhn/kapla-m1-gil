059c3008a343a8ac7bf771ccef39fe365c896cc8
folabi41 <folabi41@b3bcd212-3c03-7b25-41cb-3a5433dfaa12>
2013-04-30 11:59:31 +0000

    Boite de dialogue centrée

--- EditorKapla/src/model/notice/CommentDefinition.java
+++ EditorKapla/src/model/notice/CommentDefinition.java
@@ -1,6 +1,8 @@
 package model.notice;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -30,6 +32,15 @@ public class CommentDefinition extends JDialog{
 		
 		super(owner, "Comment", true);
 		setLayout(new BorderLayout());
+		
+		// centrer
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+	    int x = (int) ((dimension.getWidth() - 
+	    		this.getWidth()) / 2);
+	    int y = (int) ((dimension.getHeight() - 
+	    		this.getHeight()) / 2);
+	    setLocation(x, y);
+		
 		
 		JPanel buttonPanel = new JPanel(new BorderLayout());
 		// bouton annuler
