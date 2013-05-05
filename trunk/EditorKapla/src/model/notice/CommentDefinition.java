149ac1e370dc55ba2f7dbfa129fb469a463d933e
folabi41 <folabi41@b3bcd212-3c03-7b25-41cb-3a5433dfaa12>
2013-05-05 20:04:00 +0000

    Changement du design de la notice.

--- trunk/EditorKapla/src/model/notice/CommentDefinition.java
+++ trunk/EditorKapla/src/model/notice/CommentDefinition.java
@@ -1,11 +1,18 @@
 package model.notice;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -21,17 +28,46 @@ import javax.swing.JTextArea;
  */
 @SuppressWarnings("serial")
 public class CommentDefinition extends JDialog{
-	
+	/**
+	 * Zone de commentaire.
+	 */
 	private JTextArea comment = new JTextArea("Tapez votre commentaire ici");
 	
 	/**
 	 * Constructeur.
-	 * @param owner frame parent.
 	 */
-	public CommentDefinition(JFrame owner, final NoticeImage noticeImage){
+	public CommentDefinition(JFrame owner, final ImageAction imageAction){
 		
-		super(owner, "Comment", true);
+		super(owner, true);
 		setLayout(new BorderLayout());
+		setSize(500, 500);
+		getContentPane().setBackground(Color.DARK_GRAY);
+		// label qui contient l'image en plus grand
+		JLabel label = null;
+		
+		// design zone de commentaire
+		comment.setRows(5);
+		comment.setBackground(Color.GRAY);
+		comment.setForeground(Color.WHITE);
+		
+		
+		final NoticeImage noticeImage = imageAction.getNoticeImage();
+		
+		// image redimensionne
+		try {
+			BufferedImage originalImage = ImageIO.read(new File(
+					noticeImage.getPath()));
+			
+			BufferedImage resizeImage = resizeImage(
+					originalImage, BufferedImage.TYPE_INT_ARGB, 350, 
+					350);
+			ImageIcon newImageIcon = new ImageIcon(resizeImage);
+
+			label = new JLabel(newImageIcon);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		// centrer
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
@@ -43,6 +79,7 @@ public class CommentDefinition extends JDialog{
 		
 		
 		JPanel buttonPanel = new JPanel(new BorderLayout());
+		buttonPanel.setBackground(Color.DARK_GRAY);
 		// bouton annuler
 		JButton annuler = new JButton("Annuler");
 		annuler.addActionListener(new ActionListener(){
@@ -65,13 +102,31 @@ public class CommentDefinition extends JDialog{
 		buttonPanel.add(new JLabel());
 		buttonPanel.add(valider, BorderLayout.EAST);
 		
+		JPanel tempPanel = new JPanel(new BorderLayout());
+		tempPanel.add(comment);
+		tempPanel.add(buttonPanel, BorderLayout.SOUTH);
+		
 		// ajout des composants.
-		add(comment);
-		add(buttonPanel, BorderLayout.SOUTH);
+		add(label);
+		add(tempPanel, BorderLayout.SOUTH);
+	}
+	
+	/**
+	 * Redimensionne une image. 
+	 * @param originalImage l'image d'origine
+	 * @param type type des variables width et height
+	 * @param width largeur de l'image
+	 * @param height hauteur de l'image
+	 * @return une image redimensionnee.
+	 */
+	private static BufferedImage resizeImage(BufferedImage originalImage, 
+			int type, int width, int height){
 		
-		
-		pack();
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setVisible(true);
+		BufferedImage resizedImage = new BufferedImage(width, height, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.drawImage(originalImage, 0, 0, width, height, null);
+		g.dispose();
+
+		return resizedImage;	
 	}
 }
