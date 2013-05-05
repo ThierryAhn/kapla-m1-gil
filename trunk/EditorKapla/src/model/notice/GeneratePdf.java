e6d1f90d666044c36cb180c17074588b35d51158
folabi41 <folabi41@b3bcd212-3c03-7b25-41cb-3a5433dfaa12>
2013-05-05 20:52:57 +0000

    Version finale de la notice, avec nouveau rendu pdf.

--- trunk/EditorKapla/src/model/notice/GeneratePdf.java
+++ trunk/EditorKapla/src/model/notice/GeneratePdf.java
@@ -6,8 +6,11 @@ import java.net.MalformedURLException;
 import java.util.ArrayList;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class GeneratePdf {
@@ -48,20 +51,27 @@ public class GeneratePdf {
 				IOException{
 		Document document = new Document();
 		PdfWriter.getInstance(document, 
-				new FileOutputStream("Fichier.pdf"));
+				new FileOutputStream("Notice.pdf"));
 		document.open();
+		
+		Paragraph paragraph = new Paragraph("Notice");
+		paragraph.setAlignment(Element.ALIGN_CENTER);
+		document.add(paragraph);
+		document.add(new Paragraph("\n\n"));
+		
+		PdfPTable table = new PdfPTable(2);
 		
 		// recuperations des images et ecriture dans le fichier pdf
 		for(NoticeImage noticeImage : arrayImages){
 			if(noticeImage.isSelected()){
 				Image image = Image.getInstance(noticeImage.getPath());
-				image.scaleAbsolute(300, 300);
-				document.add(image);
-				document.add(new Phrase("\n"));
-				document.add(new Phrase(noticeImage.getComment()));
-				document.add(new Phrase("\n"));
+				image.scaleAbsolute(200, 200);
+				
+				table.addCell(image);
+				table.addCell(new Phrase(noticeImage.getComment()));
 			}
 		}
+		document.add(table);
 		document.close();
 	}
 }
